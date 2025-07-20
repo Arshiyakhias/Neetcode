96793bd051f47625c9fb5f58254e37f0fd2d6bcd
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-20 17:34:36 +0530

    Added k-closest-points-to-origin on 2025-07-20

--- 2025-07-20/k-closest-points-to-origin/solution.java
+++ 2025-07-20/k-closest-points-to-origin/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int[][] kClosest(int[][] points, int k) {
         List<List<Integer>>lis=new ArrayList<>();
         for(int[]arr:points){
-            ArayList<Integer>addon=new ArrayList<>();
+            List<Integer>addon=new ArrayList<>();
             for(int i=0;i<arr.length;i++){
                 addon.add(arr[i]);
             }

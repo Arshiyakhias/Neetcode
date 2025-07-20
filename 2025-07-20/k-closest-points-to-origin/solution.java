3cba71232b663f074826c7eb35b63cc77d1874ac
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-20 17:42:23 +0530

    Added k-closest-points-to-origin on 2025-07-20

--- 2025-07-20/k-closest-points-to-origin/solution.java
+++ 2025-07-20/k-closest-points-to-origin/solution.java
@@ -4,7 +4,7 @@ class Solution {
         Integer.compare(a[0]*a[0]+a[1]*a[1],b[0]*b[0]+b[1]*b[1])
         );
         int[][]ans;
-        ans=new int[2][2];
+        ans=new int[k][2];
         for(int i=0;i<k;i++ ){
             ans[i]=points[i];
         }

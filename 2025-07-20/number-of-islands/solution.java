8b30bbc273a9f760039d9724e4cc47d86a5a0455
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-20 15:33:42 +0530

    Added number-of-islands on 2025-07-20

--- 2025-07-20/number-of-islands/solution.java
+++ 2025-07-20/number-of-islands/solution.java
@@ -12,9 +12,9 @@ class Solution {
         return count;
         
     }
-    public int dfs(char[][]grid,int i,int j){
+    public void dfs(char[][]grid,int i,int j){
         if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]=='0'){
-            return 0;
+            return ;
         }
         grid[i][j]='0';
         dfs(grid,i+1,j);

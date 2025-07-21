4876354c33c1d28fac11e7bf53014491e7da3189
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 15:50:56 +0530

    Added encode-and-decode-strings on 2025-07-21

---
+++ 2025-07-21/encode-and-decode-strings/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        
+        String enc=new String();
+        for(String word:strs){
+
+           
+            
+            enc=enc+word;
+            enc=enc+"#";
+
+        }
+        return enc;
+
+    }
+
+    public List<String> decode(String str) {
+        String[]arr=str.split("#");
+        List<String>ans=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            ans.add(arr[i]);
+        }
+        return ans;
+
+
+
+    }
+}

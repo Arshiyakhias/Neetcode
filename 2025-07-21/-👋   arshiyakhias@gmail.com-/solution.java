aa9a11f03ea9699071c86ab587db1fe3f478bd08
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 15:49:31 +0530

    Added -👋   arshiyakhias@gmail.com- on 2025-07-21

---
+++ 2025-07-21/-👋   arshiyakhias@gmail.com-/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,ArrayList<String>>map=new HashMap<>();
+        for(String word:strs){
+            String[]arr=word.split("");
+            Arrays.sort(arr);
+            String sorts=new String();
+            for(int i=0;i<arr.length;i++){
+                sorts=sorts+arr[i];
+            }
+            if(map.containsKey(sorts)){
+                map.get(sorts).add(word);
+
+            }
+            else{
+                map.put(sorts,new ArrayList<>());
+                map.get(sorts).add(word);
+            }
+
+        }
+    }
+}

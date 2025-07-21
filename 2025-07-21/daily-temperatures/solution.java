3f1ca8976afb713a5ea61056e62557d4976015b8
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 17:13:47 +0530

    Added daily-temperatures on 2025-07-21

---
+++ 2025-07-21/daily-temperatures/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer>st=newStack<>();
+        for(int i=temperatures.length-1;i>=0;i--){
+            if(st.isEmpty()){
+                st.push(temperatures[i]);
+                temperatures[i]=0;
+            }
+            if(st.peek()>temperatures[i]){
+                st.push(temperatures[i]);
+                temperatures[i]=st.peek();
+            }
+            if(st.peek()<temperatures[[i]]){
+                while(st.peek()>temperatures[i]){
+                    st.pop();
+                }
+                if(st.isEmpty()){
+                    st.push(temperatures[i]);
+                    temparatures[i]=0;
+
+                }
+                else{
+                    st.push(temperatures[i]);
+                    temparatures[i]=st.peek();
+
+                }
+            }
+        }
+        return temperatures;
+    }
+}

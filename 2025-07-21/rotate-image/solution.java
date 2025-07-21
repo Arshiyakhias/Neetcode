c8af6151cde33ad7d0183f34094d81943aaa1910
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 18:50:50 +0530

    Added rotate-image on 2025-07-21

---
+++ 2025-07-21/rotate-image/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(i!=j && i<j){
+                    int temp=matrix[i][j];
+                    int matrix[i][j]=matrix[j][i];
+                    matrix[j][i]=matrix[i][j];
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length/2;j++){
+                int temp1=matrix[i][j];
+                int matrix[i][j]=matrix[i][matrix.length-j-1];
+                matrix[i][matrix.length-j-1]=temp1;
+            }
+        }
+    }
+}

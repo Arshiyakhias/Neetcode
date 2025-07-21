987117452b57031b94b272d0482e1ed4d444678e
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 15:27:56 +0530

    Added reverse-bits on 2025-07-21

---
+++ 2025-07-21/reverse-bits/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        String bin=new String();
+        while(n!=0){
+            int fin=n%10;
+            bin=bin+fin;
+            n=n/10;
+        }
+        return Integer.toBinary(bin);
+    }
+}

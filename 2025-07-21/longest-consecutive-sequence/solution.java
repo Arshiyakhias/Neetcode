30e7147f0bc7b19dc176307fab800df05ad92dfd
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 22:19:03 +0530

    Added longest-consecutive-sequence on 2025-07-21

---
+++ 2025-07-21/longest-consecutive-sequence/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        ArrayList<Integer>lis=new ArrayList<>(set.values());
+    }
+}

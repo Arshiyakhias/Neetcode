fefb6b27c8755aca143954fa2a3ca44d3659fbb5
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 22:13:12 +0530

    Added merge-two-sorted-linked-lists on 2025-07-21

--- 2025-07-21/merge-two-sorted-linked-lists/solution.java
+++ 2025-07-21/merge-two-sorted-linked-lists/solution.java
@@ -11,7 +11,11 @@
 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null || list2==null){
+        if(list1==null){
+            return list1;
+
+        }
+        if(list1==null && list2==null){
             return list1;
 
         }

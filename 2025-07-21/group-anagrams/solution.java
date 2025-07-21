11659a81e6114b2bfbe9b166f880c06b803b95e6
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 15:41:27 +0530

    Added group-anagrams on 2025-07-21

---
+++ 2025-07-21/group-anagrams/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,ArrayList<String>>map=new HashMap<>();
+        for(word:strs){
+            Character[]arr=word.split('');
+            
+        }
+    }
+}

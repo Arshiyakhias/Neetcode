d5513134796531257205793b8ec9874fbb524263
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-21 17:32:53 +0530

    Added daily-temperatures on 2025-07-21

--- 2025-07-21/daily-temperatures/solution.java
+++ 2025-07-21/daily-temperatures/solution.java
@@ -25,7 +25,7 @@ class Solution {
                 else{
                     temperatures[i]=st.peek();
                     st.push(temperatures[i]);
-                    temperatures[i]=st.peek();
+                    
 
                 }
             }

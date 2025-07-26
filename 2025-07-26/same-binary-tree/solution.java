2d6f9e3b020117ccb5cd9336ee9f719c23cf5e34
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-26 17:58:56 +0530

    Added same-binary-tree on 2025-07-26

---
+++ 2025-07-26/same-binary-tree/solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public void inorder(List<Integer>lis,TreeNode root){
+        if(root==null){
+            lis.add(null);
+            return;
+        }
+        inorder(lis,root.left);
+        lis.add(root.val);
+        inorder(lis,root.right);
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        ArrayList<Integer>first=new ArrayList<>();
+        ArrayList<Integer>second=new ArrayList<>();
+        inorder(first,p);
+        inorder(second,q);
+        for(int i=0;i<first.size();i++){
+            if(first.get(i)!=secon.get(i)){
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+}

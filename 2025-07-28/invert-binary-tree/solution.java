d8bbf86207fd1c588356a142218a0c3b79bd69f5
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-07-28 15:56:12 +0530

    Added invert-binary-tree on 2025-07-28

--- 2025-07-28/invert-binary-tree/solution.java
+++ 2025-07-28/invert-binary-tree/solution.java
@@ -26,7 +26,7 @@ class Solution {
 
 
     }
-    private static inorder(List<Integer>lis,TreeNode root,int count){
+    private static invert(List<Integer>lis,TreeNode root,int count){
         if(root==null){
 
             return;

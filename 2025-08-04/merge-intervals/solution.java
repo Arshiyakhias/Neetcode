1f4a344bac00caeabba59a2a33eeb3a8d66c66fa
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-08-04 21:25:43 +0530

    Added merge-intervals on 2025-08-04

--- 2025-08-04/merge-intervals/solution.java
+++ 2025-08-04/merge-intervals/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals);
+        //Arrays.sort(intervals);
         List<List<Integer>>lis=new ArrayList<>();
         for(int i=0;i<intervals.length;i++){
             if(intervals[i][1]>=intervals[i+1][0] && i!=intervals.length-1){

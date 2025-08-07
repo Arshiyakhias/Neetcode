ac4619414e0e12cd1282e12707cac234e74b79cb
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-08-08 01:25:25 +0530

    Added min-cost-climbing-stairs on 2025-08-07

---
+++ 2025-08-07/min-cost-climbing-stairs/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[]ans;
+        ans=new int[cost.length];
+        if(cost.length==1){
+            return cost[0];
+        }
+        if(cost.length==2){
+            if(cost[0]>cost[1]){
+                return cost[1];
+            }
+            else{
+                return cost[0];
+            }
+        }
+        ans[0]=cost[0];
+        ans[1]=cost[1];
+        for(int i=2;i<cost.length;i++){
+            ans[i]=ans[i-1]+cost[i];
+
+        }
+        if(ans[ans.length-1]>ans[ans.length-2]){
+            return ans[ans.length-2];
+        }
+        return ans[ans.length-1;]
+
+
+    }
+}

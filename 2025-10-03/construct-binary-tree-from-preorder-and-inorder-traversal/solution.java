9d22dc58c3124c90e788a68f1449874189865939
Arshiya Khiasudeen <arshiyakhias@gmail.com>
2025-10-03 20:56:11 +0530

    Added construct-binary-tree-from-preorder-and-inorder-traversal on 2025-10-03

---
+++ 2025-10-03/construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int index=0;
+    private int poscal(int[]inorder,int vals){
+        for(int i=0;i<inorder.length;i++){
+            if(vals==inorder[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private TreeNode solve(int[]preorder,int[]inorder,int inorderStart,int inorderEnd){
+        if(inorderStart>inorderEnd || index>preoder.length){
+            return null;
+        }
+
+        TreeNode ans=new TreeNode(preorder[index]);
+        index++;
+        int pos=poscal(inorder,addon.val);
+        ans.left=solve(int[]preorder,int[]inorder,0,pos-1);
+        ans.right=solve(int[]preorder,int[]inorder,pos+1,inorder.length-1);
+        return ans;
+
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        TreeNode ans=solve(int[]preorder,int[]inorder,0,inorder.length-1);
+        return ans;
+        
+    }
+}
